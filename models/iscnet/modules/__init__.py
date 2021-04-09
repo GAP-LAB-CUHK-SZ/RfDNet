6f4f3591a5f8b52108824b43674a3db66cdfb44b
ynie <nieyinyu0731@gmail.com>
2021-04-09 13:03:23 +0100

    Fixed bugs and updated Readme.

--- models/iscnet/modules/__init__.py
+++ models/iscnet/modules/__init__.py
@@ -2,13 +2,7 @@ from .network import ISCNet
 from .pointnet2backbone import Pointnet2Backbone
 from .proposal_module import ProposalModule
 from .vote_module import VotingModule
-from .pcn_ae import PCN_Autoencoder
 from .occupancy_net import ONet
 from .skip_propagation import SkipPropagation
-from .MLCV_proposal_module import MLCV_ProposalModule
-from .MLCV_vote_module import MLCV_VotingModule
-from .DGCNN_proposal_module import DGCNN_ProposalModule
-from .second_seg import Seg_2nd
 
-__all__ = ['ISCNet', 'Pointnet2Backbone', 'ProposalModule', 'VotingModule', 'PCN_Autoencoder', 'ONet', 'SkipPropagation',
-           'MLCV_ProposalModule', 'MLCV_VotingModule', 'DGCNN_ProposalModule', 'Seg_2nd']
+__all__ = ['ISCNet', 'Pointnet2Backbone', 'ProposalModule', 'VotingModule', 'ONet', 'SkipPropagation']
